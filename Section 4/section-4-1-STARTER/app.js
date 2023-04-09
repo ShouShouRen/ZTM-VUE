474071a896c5fc30a4940d6ac6018eca32df781a
Peter Chen <peterchen2017125@gmail.com>
2023-04-09 14:14:34 +0800

    20230409

--- Section 4/section-4-1-STARTER/app.js
+++ Section 4/section-4-1-STARTER/app.js
@@ -1,12 +1,30 @@
 let vm = Vue.createApp({
   data() {
     return {
-      message: "Hello world!"
-    }
-  }
-})
+      message: "Hello world!",
+    };
+  },
+  beforeCreate() {
+    console.log("beforeCreate() function called", this.message);
+  },
+  created() {
+    console.log("Create() function called", this.message);
+  },
+  beforeMount() {
+    console.log("beforMount() function called", this.$el);
+  },
+  mounted() {
+    console.log("Mounted() function called", this.$el);
+  },
+  beforeMount() {
+    console.log("beforMount() function called");
+  },
+  update() {
+    console.log("update() function called");
+  },
+});
 
-vm.mount('#app')
+vm.mount("#app");
 // setTimeout(() => {
 //   vm.mount('#app')
 // }, 3000);
