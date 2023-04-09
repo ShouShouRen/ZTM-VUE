b459755bfa5444bc94169c1e0c24e55ac4754ed2
Peter Chen <peterchen2017125@gmail.com>
2023-04-09 14:46:21 +0800

    20230409

--- Section 4/section-4-1-STARTER/app.js
+++ Section 4/section-4-1-STARTER/app.js
@@ -4,27 +4,45 @@ let vm = Vue.createApp({
       message: "Hello world!",
     };
   },
-  beforeCreate() {
-    console.log("beforeCreate() function called", this.message);
-  },
-  created() {
-    console.log("Create() function called", this.message);
-  },
-  beforeMount() {
-    console.log("beforMount() function called", this.$el);
-  },
-  mounted() {
-    console.log("Mounted() function called", this.$el);
-  },
-  beforeMount() {
-    console.log("beforMount() function called");
-  },
-  update() {
-    console.log("update() function called");
-  },
+  // beforeCreate() {
+  //   console.log("beforeCreate() function called", this.message);
+  // },
+  // created() {
+  //   console.log("Create() function called", this.message);
+  // },
+  // beforeMount() {
+  //   console.log("beforMount() function called", this.$el);
+  // },
+  // mounted() {
+  //   console.log("Mounted() function called", this.$el);
+  // },
+  // beforeMount() {
+  //   console.log("beforMount() function called");
+  // },
+  // update() {
+  //   console.log("update() function called");
+  // },
+  template: `{{ message }}`
 });
 
 vm.mount("#app");
+
+let vm2 = Vue.createApp({
+  data() {
+    return {
+      message: "Hello world!",
+    };
+  },
+  render() {
+    return Vue.h(
+      'h1',
+      this.message
+    )
+  }
+}).mount("#app2");
+
+
+
 // setTimeout(() => {
 //   vm.mount('#app')
 // }, 3000);
