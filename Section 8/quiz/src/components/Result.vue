6e7cc26036e59171efc4f3d0a3a5fab752616bee
Peter Chen <peterchen2017125@gmail.com>
2023-08-01 17:05:19 +0800

    20230801

---
+++ Section 8/quiz/src/components/Result.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+  props: ["results", "totalCorrect"],
+  computed: {
+    resultIndex() {
+      let index = 0;
+      return index;
+    },
+  }
+}
+</script>
+
+<template>
+  <div class="result">
+    <div class="title">You got sample result 1!</div>
+    <div class="desc">
+      Enter a short description here about the result.
+    </div>
+  </div>
+</template>
+
+<style></style>
